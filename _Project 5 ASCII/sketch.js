085d55972e085f539ae1d3537b8e55a83dbd0197
aarlington <aarlington@aucklandmuseum.com>
2025-12-11 13:09:01 +1300

    ascii added

---
+++ _Project 5 ASCII/sketch.js
@@ -0,0 +1,21 @@
+const density = 'Ñ@#W$9876543210?!abc;:+=-,._ '
+let heart
+
+
+
+function setup() {
+  createCanvas(20, 20);
+  loadPixels();
+  noLoop();
+
+}
+
+function draw() {
+background(205)
+  
+  for (let i = 0; i <width; i++ ){
+    for (let j = 0; j <width; j++){
+      const pixelNumber = (i + j * width)  * 4
+    }
+  }
+}
